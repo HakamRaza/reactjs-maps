b43018f027ee7685294e85212c49e29b7f5633fd
HakamRaza <ismatcd@yahoo.com>
2023-05-15 16:23:15 +0800

    feat search input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import {
+	debounceTime,
+	map,
+	BehaviorSubject,
+	filter,
+	distinctUntilChanged
+} from 'rxjs';
+import {
+	Input,
+} from 'antd';
+
+
+const MAPBOX_BASE_URL = 'https://api.mapbox.com'
+const MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN
+const MAPBOX_UUID4 = process.env.UUID4
+const { Search } = Input;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [subject, setSubject] = useState(null);
+	const [state, setState] = useState({
+		searchString: [],
+		suggestionList: [],
+		suggestionDetail: {
+			name: '',
+			full_address: '',
+			coordinate_array: [0, 0],
+			latitude: 0,
+			longitude: 0,
+		},
+		loading: false
+	});
+
+	useEffect(() => {
+		if (subject === null) {
+			const sub = new BehaviorSubject('');
+			setSubject(sub);
+		} else {
+			const observable = subject.pipe(
+				map(searchStr => searchStr.trim()),
+				distinctUntilChanged(),
+				filter(searchStr => searchStr.length >= 4),
+				debounceTime(2000)
+			)
+				.subscribe(searchStr => {
+					return axios.get(`${MAPBOX_BASE_URL}/search/searchbox/v1/suggest`, {
+						params: {
+							q: searchStr,
+							access_token: MAPBOX_ACCESS_TOKEN ?? '',
+							session_token: MAPBOX_UUID4 ?? '',
+							limit: '5',
+							language: 'en'
+						}
+					})
+						.then(({ data }) => {
+							const results = []
+
+							for (const suggestion of data.suggestions) {
+								results.push({
+									name: suggestion.name,
+									mapbox_id: suggestion.mapbox_id,
+									full_address: suggestion.full_address,
+								})
+							}
+
+							const newState = {
+								...state,
+								searchString: [...state.searchString, searchStr],
+								suggestionList: results,
+								suggestionDetail: {
+									name: '',
+									full_address: '',
+									coordinate_array: [0, 0],
+									latitude: 0,
+									longitude: 0,
+								},
+								loading: false
+							}
+							setState(newState)
+						});
+				});
+
+			return () => {
+				observable.unsubscribe()
+				subject.unsubscribe();
+			}
+		}
+	}, [subject]);
+
+	const onSearch = () => {
+		subject.next('Kota bharu')
+	}
+
+	return (
+		<>
+			<Search placeholder="input search text" onSearch={onSearch} enterButton size='middle' />
+		</>
+	);
 }
 
 export default App;
